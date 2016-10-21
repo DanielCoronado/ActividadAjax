3b9a9578ec569fb33e099fc644342036cf3405fd
PedroManriquez <ptrmanriquez@gmail.com>
2016-10-20 23:19:44 -0300

    version funcional para taller

--- ajax/read.js
+++ ajax/read.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $.get('../server/read.php', function (data) {
-      console.log(data);
+      if (data.exito) {
+        for (var i = 0; i < data.datos.length; i++) {
+          console.log (data.datos[i]);
+        }
+      }
     });
 });
